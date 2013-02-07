18454cca020e522c90f699189f144ea60d28e7a9
Matt Mets <matt.mets@cibomahto.com>
2013-02-07 12:51:48 +0800

    Improve minimum on time to achieve darker levels.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,10 +1,8 @@
 //#include <avr/interrupt.h>
-
 #include "SoftPWM.h"
 #include "Protocol.h"
 
 #define PIN_DE           3    // Set this high to enable transmission on the RS485 line
-#define PIN_LED_DEBUG    11
 
 SoftPWM softPWM;
 Protocol receiver;
@@ -13,14 +11,12 @@ void setup() {
   //  pinMode(PIN_DE, OUTPUT);  // enable RS485 input
   //  digitalWrite(PIN_DE, 0);
 
-  //  pinMode(PIN_LED_DEBUG, OUTPUT);
-  //  digitalWrite(PIN_LED_DEBUG, LOW);
-
   Serial.begin(57600);
   receiver.reset();
 
   softPWM.begin();
-  softPWM.setPWM(0, 100);
+  softPWM.setPWM(0, 1);
+  softPWM.setPWM(1, 2);
 }
 
 void loop() {
